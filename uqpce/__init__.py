82fbde11891b0b33cd1a500f4effbebc570f4f9f
Joanna Schmidt <joanna.n.schmidt@nasa.gov>
2026-08-06 10:20:07 -0400

    updated naming convention of UQPCEGroup and MultiUQPCEGroup

--- uqpce/__init__.py
+++ uqpce/__init__.py
@@ -1,8 +1,9 @@
 __version__ = '1.0.1-dev'
 
 from uqpce.pce.pce import PCE
+
 try:
-    from uqpce.mdao.uqpcegroup import UQPCEGroup
     from uqpce.mdao import interface
+    from uqpce.mdao.uqpcegroup import MultiUQPCEGroup, UQPCEGroup
 except:
     pass # openmdao not installed, which is fine if analysis-only is desired
